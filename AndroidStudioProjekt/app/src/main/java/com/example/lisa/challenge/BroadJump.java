62ee2a5435d06cde4f06444ae3899a9db39f53c2
Haefner <lisa-maria.haefner@hs-bochum.de>
2018-04-30 21:37:38 +0200

    Design korrigiert

--- AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/BroadJump.java
+++ AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/BroadJump.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class BroadJump extends Activity {
+public class BroadJump extends AppCompatActivity {
 
     Button navigateToMainMenu;
 
--- AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/Rotate.java
+++ AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/Rotate.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class Rotate extends Activity {
+public class Rotate extends AppCompatActivity {
 
     Button navigateToMainMenu;
 
