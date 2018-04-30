6f44f0676966551dafb06bbffb18add5d0391cd0
Haefner <lisa-maria.haefner@hs-bochum.de>
2018-04-30 22:45:10 +0200

    Design korrigiert

--- AndroidStudioProjekt/app/src/androidTest/java/com/example/lisa/challenge/ExampleInstrumentedTest.java
+++ AndroidStudioProjekt/app/src/androidTest/java/com/example/lisa/challenge/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.lisa.challange", appContext.getPackageName());
+        assertEquals("com.example.lisa.challenge", appContext.getPackageName());
     }
 }
