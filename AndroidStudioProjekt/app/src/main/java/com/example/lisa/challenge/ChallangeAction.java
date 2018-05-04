617b77751eadc83d60e6b6a7a262cfceee68e7c6
Haefner <lisa-maria.haefner@hs-bochum.de>
2018-05-04 15:40:43 +0200

    Messsensoren aktiviert

--- AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/ChallangeAction.java
+++ AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/ChallangeAction.java
@@ -2,7 +2,6 @@ package com.example.lisa.challenge;
 
 import android.media.AudioManager;
 import android.media.ToneGenerator;
-import android.os.CountDownTimer;
 import android.util.Log;
 
 import java.util.Collections;
@@ -11,8 +10,6 @@ import java.util.List;
 public class ChallangeAction {
 
        final ToneGenerator sound = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
-
-
 
     /**
      * Wenn der Lichtwert unter 10 ist (es ist Dunkel) wird true zurückgegeben, sonst false
@@ -30,37 +27,13 @@ public class ChallangeAction {
         }
         //Maximum ermitteln
         Float max = Collections.max(messwerte);
+        Log.d("runnable","Maximaler Messwert "+ max + "- startwert"+ startValue);
 
         if(max<startValue)
         {
             return 0;
         }
-        return startValue-max;
+        return max- startValue;
     }
-
-    /**
-     * Startet einen Countdown und gibt jede Sekunge einen Sound wieder
-     * @param seconds Zu wartende Zeit in Sekunden
-     */
-    public void startCountdown(int seconds)
-    {
-
-        new CountDownTimer(seconds*1000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                Log.d("runnable","Countdown läuft");
-                sound.startTone(ToneGenerator.TONE_PROP_BEEP);
-
-            }
-
-            public void onFinish() {
-                Log.d("runnable","Countdown beendet");
-                sound.startTone(ToneGenerator.TONE_PROP_BEEP2);
-            }
-
-
-        }.start();
-    }
-
 
 }
--- AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/HighJump.java
+++ AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/HighJump.java
@@ -7,15 +7,9 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class HighJump extends AppCompatActivity {
 
@@ -29,11 +23,10 @@ public class HighJump extends AppCompatActivity {
     TextView result;
 
     //Gibt an, ob die Aufnahme gestartet worden ist.
-    Boolean startMeasure;
+    private Boolean startMeasure;
     //Gibt an, ob das Handy sich in der Hosentasche befindet und die Umgebung dunkel ist
-    Float hoeheAusgangspunkt = 0f;
-    Float aktuelleHoehe = 0f;
-    Float aktuellerLichwert = 100f;
+    private Float aktuelleHoehe = 0f;
+    private Float aktuellerLichwert = 100f;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,7 +39,6 @@ public class HighJump extends AppCompatActivity {
         setUpSensorListener();
         setUpListenerButtons();
     }
-    SensorEvent sensorEvent;
 
     private void setUpSensorListener() {
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -58,25 +50,20 @@ public class HighJump extends AppCompatActivity {
 
             @Override
             public void onSensorChanged(SensorEvent event) {
-                sensorEvent=event;
                 switch (event.sensor.getType()) {
                     //Bewegungssensor Liniar
                     case Sensor.TYPE_ACCELEROMETER:
-                        Log.d("runnable", "Hoehe " + event.values[1]);
                         aktuelleHoehe = event.values[1];
                         break;
                     //Lichsensor
                     case Sensor.TYPE_LIGHT:
-                        Log.d("runnable", "Lichtwert" + event.values[0]);
                         aktuellerLichwert = event.values[0];
-                        result.setText("Aktueller Lichwert " + aktuellerLichwert);
                         break;
                 }
-            }
 
+            }
         };
     }
-
 
     private void setUpListenerButtons() {
         start.setOnClickListener(new View.OnClickListener() {
@@ -84,7 +71,7 @@ public class HighJump extends AppCompatActivity {
             public void onClick(View v) {
                 cancel.setVisibility(View.VISIBLE);
                 start.setVisibility(View.GONE);
-                startMeasure = true;
+                setStartMeasure(true);
                 actionHighJump();
             }
         });
@@ -93,7 +80,7 @@ public class HighJump extends AppCompatActivity {
             public void onClick(View v) {
                 start.setVisibility(View.VISIBLE);
                 cancel.setVisibility(View.GONE);
-                startMeasure = false;
+                setStartMeasure(false);
             }
         });
     }
@@ -102,61 +89,77 @@ public class HighJump extends AppCompatActivity {
         start = findViewById(R.id.buttonStartHighJump);
         cancel = findViewById(R.id.buttonCancelHighJump);
         result = findViewById(R.id.textResult);
-
-
     }
 
     private void actionHighJump() {
 
-         SensorThreads sensorThreads = new SensorThreads(sensorManager, sensorEventListener, Arrays.asList(SensorTyp.LIGHT, SensorTyp.ACCELEROMETER));
-         sensorThreads.execute();
+        registerListener(SensorTyp.LIGHT, 10000);
+        registerListener(SensorTyp.ACCELEROMETER, 10000);
+        HighJumpBarechnenThread thread = new HighJumpBarechnenThread(this, challange);
+        thread.execute();
+//        Float ergebniss = 0f;
+//        try {
+//            ergebniss = (Float) thread.execute().get();
+//            result.setText("Du bist " + ergebniss + "mm hoch gesprungen");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // /Am Ende der Messung den Cancel Button wieder deaktivieren
+//        cancel.setVisibility(View.GONE);
+//        start.setVisibility(View.VISIBLE);
+//        startMeasure=false;
+//        deaktivateListener(SensorTyp.LIGHT);
+//        deaktivateListener(SensorTyp.ACCELEROMETER);
+    }
 
-        //Warte so lange bis das Smartphone in der Hosentasche ist und die Messung läuft
-        while (!challange.isDark(aktuellerLichwert)&& startMeasure) {
-            try {
-                Log.d("runnable", "Warte das Handy in die Hosentesche gesteckt wird. Aktueller Lichtewert = " + aktuellerLichwert);
-                TimeUnit.SECONDS.sleep(1);
+    public Float getAktuelleHoehe() {
+        return aktuelleHoehe;
+    }
 
+    public Float getAktuellerLichwert() {
+        return aktuellerLichwert;
+    }
 
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        //Die Messung wurde abgebrochen
-        if (startMeasure == false) {
-            return;
-        }
-
-
-        //Bestimme die Bezugshöhe von der die Person abspringt
-        hoeheAusgangspunkt = aktuelleHoehe;
-
-        challange.startCountdown(3);
-
-        // Solange es dunkel ist, und maximal 4 Sekunden Werte messen
-        List<Float> messwerte = new ArrayList<>();
-        long t = System.currentTimeMillis();
-        long end = t + 4000;
-        Log.d("runnalbe", "starte Aufzeichnung der Werte");
-        while (challange.isDark(aktuellerLichwert) && startMeasure && System.currentTimeMillis() < end) {
-            messwerte.add(aktuelleHoehe);
-        }
-
-        if (startMeasure) {
-            Log.d("runnable", "berechne Ergebniss");
-            float ergebniss = challange.getDiffenenzOfStartValueAndMax(hoeheAusgangspunkt, messwerte);
-            Log.d("runnable", "Du bist " + ergebniss + "mm hoschgesprungen");
-            result.setText("Du bist " + ergebniss + "mm hoch gesprungen");
-        }
-
-        //Am Ende der Messung den Cancel Button wieder deaktivieren
-        cancel.setVisibility(View.GONE);
-        start.setVisibility(View.VISIBLE);
-        startMeasure = false;
-         sensorThreads.stopSenor();
-
+    public Boolean getStartMeasure() {
+        return startMeasure;
     }
 
 
+    public void setStartMeasure(Boolean startMeasure) {
+        this.startMeasure = startMeasure;
+    }
+
+    /**
+     * Aktiviert das Messen der Sensordaten
+     *
+     * @param sensorTyp
+     * @param time      Zeitintervall in Microsekunden, in denen die Werte abgefragt werden
+     */
+    public void registerListener(SensorTyp sensorTyp, int time) {
+        if (sensorTyp == SensorTyp.ACCELEROMETER) {
+            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), time);
+        } else if (sensorTyp == SensorTyp.GYROSCOPE) {
+            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), time);
+        } else if (sensorTyp == SensorTyp.LIGHT) {
+            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), time);
+        } else {
+            throw new RuntimeException("SensorTyp is not defined");
+        }
+    }
+
+    public void deaktivateListener(SensorTyp sensorTyp) {
+        if (sensorTyp == SensorTyp.ACCELEROMETER) {
+            sensorManager.unregisterListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+        } else if (sensorTyp == SensorTyp.GYROSCOPE) {
+            sensorManager.unregisterListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE));
+        } else if (sensorTyp == SensorTyp.LIGHT) {
+            sensorManager.unregisterListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT));
+        } else {
+            throw new RuntimeException("SensorTyp is not defined");
+        }
+    }
 
 }
