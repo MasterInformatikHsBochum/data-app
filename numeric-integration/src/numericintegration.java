69237f331e6e3fd3b7f153626d8917769de1b005
s00ng <s00ng@notexist.me>
2018-05-11 11:40:37 +0200

    integral-berechnung hinzugefügt

---
+++ numeric-integration/src/numericintegration.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class numericintegration {
+    public static double Simpsonrule_weg(LinkedList<Vector<Double>> messwerte){
+        double sum=0;
+        for(int i= 0; i< messwerte.size()-1;i++){
+            sum += (messwerte.get(i).get(0)+4*messwerte.get(i+1).get(0)+messwerte.get(i+2).get(0));
+
+            }
+        return ((messwerte.get(i).get(1)-messwerte.get(i).get(0))/3)*sum;
+    }
+
+}
