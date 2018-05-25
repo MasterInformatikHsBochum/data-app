441a4265fbd563a3bd7c063bc7f7dcc9e1e92aa0
haefner <lisa-maria.haefner@hs-bochum.de>
2018-05-25 12:30:23 +0200

    Menüpunkte umbenannt

--- AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/MainActivity.java
+++ AndroidStudioProjekt/app/src/main/java/com/example/lisa/challenge/MainActivity.java
@@ -83,17 +83,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_broadJump) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_highJump) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_rotate) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_impressum) {
 
         }
 
